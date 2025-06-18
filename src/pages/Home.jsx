f38492acdd778ccc220243b0053ebba19680bcd8
Nayan-WeatherAPI <duttanayan1919@gmail.com>
2025-06-18 20:09:46 +0530

    Home.jsx created

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import { useNavigate } from 'react-router-dom';
+
+const Home = () => {
+    return (
+        <div className='card'>
+            <h2>Welcome to PoPX</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+            <button className="btn-primary" onClick={() => navigate('/register')}>Create Account</button>
+            <button className="btn-secondary" onClick={() => navigate('/login')}>Already Registered? Login</button>
+        </div>
+    );
+};
+
+export default Home;
